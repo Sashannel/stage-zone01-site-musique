81e7b1dfe25d983d8c962acdb862cfcb22ba97b4
Sashannel <143262238+Sashannel@users.noreply.github.com>
2024-06-21 15:17:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-/* Just for example */
-console.log("Hello There!");
+// Création d'un élément audio
+let audio = new Audio('chemin/vers/ton/fichier/audio.mp3');
+
+// Création d'un bouton
+let bouton = document.createElement('button');
+bouton.textContent = 'Jouer de la musique';
+
+// Ajout d'un événement au clic sur le bouton
+bouton.addEventListener('click', function() {
+audio.play(); // Jouer la musique quand le bouton est cliqué
+});
+
+// Ajout du bouton à la page (par exemple au corps du document)
+document.body.appendChild(bouton);
