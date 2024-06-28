17d75a6ce8a77597e19c8449f8fa6e91b1274f43
Sashannel <143262238+Sashannel@users.noreply.github.com>
2024-06-28 09:17:25 +0200

    Create script.js

---
+++ version-finale/script.js
@@ -0,0 +1,19 @@
+function changeState(x){
+    let btns = document.querySelectorAll(".btnsaudio")
+    let audio = document.querySelector("#myAudio")
+    for (let i = 0; i < btns.length; i++){
+        btns[i].classList.remove("active");
+    }
+    btns[x].classList.add("active");
+    if (x == 0){
+        audio.play()
+  
+    }
+    if (x==1){
+        audio.pause()
+    }
+    if (x==2){
+        audio.pause()
+        audio.currentTime = 0
+    }
+  }
